77a8c1daf1a67834cb2609aa6834010edba3c5be
Hayden Langelier <email": null>
2016-07-21 10:12:23 -0700

    done

--- teacher.rb
+++ teacher.rb
@@ -1,21 +1,17 @@
-class Teacher
-  attr_reader :age, :salary, :phase, :performance_rating, :target_raise
-  attr_accessor :name
+require_relative 'spec/person'
+require_relative 'spec/teach'
+
+class Teacher< Person
+  include Teach
+  attr_reader :performance_rating
+  RATING=90
+  # FIRST=", fo SHO!"
+  # SECOND="flat-out"
+  # THIRD="*saunters away*"
 
   def initialize(options={})
-    @phase = 3
-    @age = options.fetch(:age, 0)
-    @name = options.fetch(:name, "")
+    super
     @target_raise = 1000
-  end
-
-  def offer_high_five
-    "High five!"
-  end
-
-  def set_phase(num)
-    @phase = num
-    "Cool, I've always wanted to teach phase #{num}!"
   end
 
   def teach_stuff
@@ -26,24 +22,11 @@ class Teacher
     response
   end
 
-  def salary=(new_salary)
-    puts "This better be good!"
-    @salary = new_salary
-  end
-
-  def receive_raise(raise)
-    @salary += raise
-  end
-
-  def set_performance_rating(rating)
-    response = ""
-    if rating > 90
-      receive_raise(@target_raise)
-      response = "Yay, I'm a great employee!"
-    else
-      response += "Oh, well -- thanks to this actionable, specific, and kind "
-      response += "feedback, I'll do better next time."
-    end
-    response
-  end
+  # def teach_stuff
+  #   response = ""
+  #   response += "Listen, class, this is how everything works#{self.class::FIRST} "
+  #   response += "*drops #{self.class::SECOND} knowledge bomb* "
+  #   response += "... You're welcome.#{self.class:THIRD}"
+  #   response
+  # end
 end
