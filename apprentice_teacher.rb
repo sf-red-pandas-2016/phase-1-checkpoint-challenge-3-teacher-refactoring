87a591fa07d192f7166600061c176a229c9bc977
amandacook <email": null>
2016-07-21 10:13:33 -0700

    Refactor all files

--- apprentice_teacher.rb
+++ apprentice_teacher.rb
@@ -1,52 +1,20 @@
-class ApprenticeTeacher
-  attr_reader :age, :salary, :phase, :target_raise
-  attr_accessor :name
+require_relative 'teach_staff'
 
-  def initialize(options={})
-    @age = options.fetch(:age, 0)
-    @name = options.fetch(:name, "")
-    @target_raise = 800
-    @phase = 3
-  end
+class ApprenticeTeacher < TeachStaff
 
-  def offer_high_five
-    "High five!"
-  end
-
-  def set_phase(num)
+  TARGET_RAISE = 800
+  def set_phase(num) #almost_same all, teach staff and student diff
     @phase = num
     "Cool, I've always wanted to teach phase #{num}!"
   end
 
-  def teach_stuff
-    response = ""
-    response += "Listen, class, this is how everything works. "
+  def teach_stuff #almost same teach staff
     response += "*drops crazy knowledge bomb* "
     response += "... You're welcome."
     response
   end
 
-  def salary=(new_salary)
-    puts "This better be good!"
-    @salary = new_salary
-  end
-
-  def receive_raise(raise)
-    @salary += raise
-  end
-
-  def set_performance_rating(rating)
-    response = ""
-    if rating > 80
-      response = "Yay, I'm a great employee!"
-      receive_raise(@target_raise)
-    else
-      response = "Oh, well -- thanks to this actionable, specific, and kind feedback, I'll do better next time."
-    end
-    response
-  end
-
-  def attend_training_session
+  def attend_training_session # diff
     puts "Whoa. I know ruby-fu"
   end
 end
