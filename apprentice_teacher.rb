5561b8bf81c7074222bd8f14f475d3847f0b9488
Jason <email": null>
2016-07-21 10:01:59 -0700

    Complete Release 1

--- apprentice_teacher.rb
+++ apprentice_teacher.rb
@@ -1,17 +1,11 @@
-require_relative 'dbc_person'
+require_relative 'dbc_teacher'
 
-class ApprenticeTeacher < DBCPerson
-  attr_reader :salary, :phase, :target_raise
+class ApprenticeTeacher < DBCTeacher
 
   def initialize(options={})
     super
     @target_raise = 800
-    @phase = 3
-  end
-
-  def set_phase(num)
-    @phase = num
-    "Cool, I've always wanted to teach phase #{num}!"
+    @raise_rating = 80
   end
 
   def teach_stuff
@@ -19,26 +13,6 @@ class ApprenticeTeacher < DBCPerson
     response += "Listen, class, this is how everything works. "
     response += "*drops crazy knowledge bomb* "
     response += "... You're welcome."
-    response
-  end
-
-  def salary=(new_salary)
-    puts "This better be good!"
-    @salary = new_salary
-  end
-
-  def receive_raise(raise)
-    @salary += raise
-  end
-
-  def set_performance_rating(rating)
-    response = ""
-    if rating > 80
-      response = "Yay, I'm a great employee!"
-      receive_raise(@target_raise)
-    else
-      response = "Oh, well -- thanks to this actionable, specific, and kind feedback, I'll do better next time."
-    end
     response
   end
 
--- teacher.rb
+++ teacher.rb
@@ -1,18 +1,11 @@
-require_relative 'dbc_person'
+require_relative 'dbc_teacher'
 
-class Teacher < DBCPerson
-  attr_reader :salary, :phase, :performance_rating, :target_raise
-
+class Teacher < DBCTeacher
+  attr_reader :performance_rating
 
   def initialize(options={})
     super
-    @phase = 3
-    @target_raise = 1000
-  end
-
-  def set_phase(num)
-    @phase = num
-    "Cool, I've always wanted to teach phase #{num}!"
+    @raise_rating = 90
   end
 
   def teach_stuff
@@ -23,24 +16,4 @@ class Teacher < DBCPerson
     response
   end
 
-  def salary=(new_salary)
-    puts "This better be good!"
-    @salary = new_salary
-  end
-
-  def receive_raise(raise)
-    @salary += raise
-  end
-
-  def set_performance_rating(rating)
-    response = ""
-    if rating > 90
-      receive_raise(@target_raise)
-      response = "Yay, I'm a great employee!"
-    else
-      response += "Oh, well -- thanks to this actionable, specific, and kind "
-      response += "feedback, I'll do better next time."
-    end
-    response
-  end
 end
